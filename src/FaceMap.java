617a13bb7f202934e363b186f4d29d6365b9ff1c
Ben Nelson <binelson13@gmail.com>
2016-01-30 10:29:58 -0600

    Playing around with JGraphx to make a FaceMap

--- src/FaceMap.java
+++ src/FaceMap.java
@@ -1,5 +1,9 @@
-import java.util.HashSet;
 import java.util.Set;
+
+import javax.swing.JFrame;
+
+import com.mxgraph.swing.mxGraphComponent;
+import com.mxgraph.view.mxGraph;
 
 /**
  * Model for a Face Map object.
@@ -7,23 +11,38 @@ import java.util.Set;
  * @author Davis Batten
  *
  */
-public class FaceMap {
+public class FaceMap extends JFrame{
 	
 	private Set<FaceElement> faces;
 	
 	//Constructor
 	public FaceMap(){
-		faces = new HashSet<FaceElement>(); //may need to change set implementation
+		mxGraph graph = new mxGraph(); 
+		Object parent = graph.getDefaultParent();
+		
+		graph.getModel().beginUpdate();
+		try {
+			Object v1 = graph.insertVertex(parent, null, "Hello", 20, 20, 80, 30);
+			Object v2 = graph.insertVertex(parent, null, "Hello", 240, 150, 80, 30);
+			graph.insertEdge(parent, null, "" , v1, v2);
+		}
+		finally {
+			graph.getModel().endUpdate();
+		}
+		
+		mxGraphComponent graphComponenet = new mxGraphComponent(graph);
+		getContentPane().add(graphComponenet);
+	}
+	
+	public static void main(String[] args) {
+		FaceMap frame = new FaceMap(); 
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(400, 320);
+		frame.setVisible(true);
 	}
 	
 	//Adds a face to the map;
 	public void addFace(FaceElement face){
 		faces.add(face);
 	}
-	
-	//Function that returns the number of faces in the map
-	public int size(){
-		return faces.size();
-	}
-	
 }
