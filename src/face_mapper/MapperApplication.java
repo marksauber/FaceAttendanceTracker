b01ba3f2b051ca037d9ac734088635c729889265
dabatten <dabatten@co1313-22.ece.iastate.edu>
2016-01-31 12:08:34 -0600

    Predicted user text added to output images

--- src/face_mapper/MapperApplication.java
+++ src/face_mapper/MapperApplication.java
@@ -2,14 +2,16 @@ package face_mapper;
 import static org.bytedeco.javacpp.opencv_imgcodecs.CV_LOAD_IMAGE_GRAYSCALE;
 import static org.bytedeco.javacpp.opencv_imgcodecs.imread;
 import static org.bytedeco.javacpp.opencv_imgcodecs.imwrite;
+import static org.bytedeco.javacpp.opencv_imgproc.*;
 
 import java.sql.Date;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.ArrayList;
 
 import org.bytedeco.javacpp.opencv_core.Mat;
+import org.bytedeco.javacpp.opencv_core.Point;
 import org.bytedeco.javacpp.opencv_core.Rect;
 import org.bytedeco.javacpp.opencv_core.RectVector;
+import org.bytedeco.javacpp.opencv_core.Scalar;
 import org.bytedeco.javacpp.opencv_face.FaceRecognizer;
 import org.bytedeco.javacpp.opencv_objdetect.CascadeClassifier;
 
@@ -33,6 +35,11 @@ public class MapperApplication {
 		String testImagePath = "testImages/davis_test.jpg";
 		FaceRecognizer faceRecognizer = FaceRecognitionUtility.newRecognizer(trainingDir);
 		
+		//Scalar colors
+		Scalar green = new Scalar(0,255,0,0);
+		//Scalar blue = new Scalar(255,0,0,0);
+		//Scalar red = new Scalar(0,0,255,0);
+		
 		//TODO get image from GUI
 		
 		Mat testImage = imread(testImagePath);
@@ -47,6 +54,8 @@ public class MapperApplication {
 		//Make a FaceMap to hold FaceElements as they are created
 		FaceMap faceMap = new FaceMap();
 		
+		ArrayList<Integer> attendance = new ArrayList<Integer>();
+		
 		//Do face recognition
 		for(int i = 0; i < faces.size(); i++){
 			//Draw detection square
@@ -55,16 +64,19 @@ public class MapperApplication {
 			
 			//Predict UserId from face
 			Mat faceMat = new Mat(testImageGray, face);
-			int predictedId = faceRecognizer.predict(faceMat); //TODO prediction accuracy?
-			String box_text = String.format("Prediction = %d", predictedId);
+			int predictedId = faceRecognizer.predict(faceMat);
+			String box_text = String.format("%s", predictedId); // TODO look up user from ID
 			System.out.println(box_text);
 			//TODO add box text to image
+			System.out.println("x:" + face.x() + "  y:" + face.y());
+			putText(testImage, box_text, new Point(face.x(), face.y() - 10), CV_FONT_HERSHEY_DUPLEX , 1.0, green);
 			
 			//Build FaceElement and add  to FaceMap
 			FaceElement fe = new FaceElement(testImage, time, predictedId, face.x(), face.y());
 			faceMap.addFace(fe);
 			
 			//TODO update attendance
+			attendance.add(predictedId);
 		}
 	
 		
